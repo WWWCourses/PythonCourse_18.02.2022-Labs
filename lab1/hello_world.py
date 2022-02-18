f9dd5f85b6c3488738da1ffcc15f862b9f6f8624
WWWCourses <ivawebcourses@gmail.com>
2022-02-18 21:04:10 +0200

    Lab1: 18.02.2022 DONE: https://wwwcourses.github.io/PythonCourseNetIT-Slides/pages/themes/PythonBasics_InstalationAndSyntax/PythonBasics_InstalationAndSyntax.html#/8

---
+++ lab1/hello_world.py
@@ -0,0 +1,13 @@
+# Python language syntax
+print('hello 9')
+print('hello 8')
+print('hello 7')
+print('hello 6')
+print('hello 5')
+print('hello 4')
+print('hello 3')
+print('hello 2')
+print('hello 1')
+
+
+
