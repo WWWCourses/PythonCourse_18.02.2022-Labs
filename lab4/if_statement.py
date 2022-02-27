e7df23ba8cb7e699fde1678e299da3b26d8f27c5
WWWCourses <ivawebcourses@gmail.com>
2022-02-27 21:03:33 +0200

    Lab4: 27.02.2022 DONE: if elif else NEXT: more on ternary operator

---
+++ lab4/if_statement.py
@@ -0,0 +1,81 @@
+# operator => '+', '<=', ...
+
+
+# ----------------------------------- TASK: ---------------------------------- #
+# if the user is adult => print('Welcome')
+# user_age = 22
+# if user_age>=18:
+# 	print('1')
+# 	print('2')
+# 	print('3')
+
+# print('4')
+
+
+# ---------------------------------- TASK2: ---------------------------------- #
+# if the user is adult (user_age>=18) => print('Welcome'), else print('Go home')
+user_age = 2
+
+# do not do that:
+# if (user_age>=18):
+# 	print('Welcome')
+
+# if not (user_age>=18):
+# 	print('Go home')
+
+# best to do:
+# if (user_age>=18):
+# 	print('Welcome')
+# else:
+# 	print('Go home')
+
+
+# ----------------------------------- elif ----------------------------------- #
+# the good way
+# condition1 = True
+# condition2 = False
+
+# if condition1:
+# 	print(1)
+# elif condition2:
+# 	print(2)
+# else:
+# 	print('None')
+
+
+# # the bad way
+# condition1 = True
+# condition2 = False
+
+# if condition1:
+# 	print(1)
+# else:
+# 	if condition2:
+# 		print(2)
+# 	else:
+# 		print('None')
+
+
+
+# ---------------------------- multiple conditions --------------------------- #
+# TASK: ако x е четно => 'even', ако x е нeчетно => 'odd', ако x == 0 =>'Zero'
+# x = 0
+# if x == 0:
+# 	print('Zero')
+# elif x%2==0:
+# 	print('even')
+# else:
+# 	print('odd')
+
+# user_age = 23
+# user_country = 'FR' # 'EN'
+
+# if user_age>=18 and user_country == 'BG':
+# 	print('Добър ден')
+# elif user_age>=18 and user_country == 'EN':
+# 	print('Welcome')
+# else:
+# 	print('END')
+
+
+
---
+++ lab4/tmp.py
@@ -0,0 +1,29 @@
+# --------------------------------- example 1 -------------------------------- #
+# input_str = 'word1 wORd2 WORd3'
+# titled_str = input_str.title()
+# print(titled_str)
+
+
+# --------------------------------- example 2 -------------------------------- #
+# input_str = 'word1 wORd2 WORd3'
+# replaced = input_str.replace('w','@')
+# print(replaced)
+
+
+# ------------------------- statement vs. expression ------------------------- #
+# expression => израз (нещо, което се изчизсля)
+# 2+3
+# 'abc'*3
+
+# print( 2+3 )
+# print( 'abc'*3 )
+
+# statements => деиствие, прави нещо, но не се изчисля
+# x = 1
+# print(  x = 1 ) # error
+
+# if 2==2:
+# 	2+3
+
+
+# print( 'a' in 'qax' )
