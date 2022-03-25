0e99760ed79c06c7644c15935b3559d67df8ea4e
WWWCourses <ivawebcourses@gmail.com>
2022-03-25 18:54:53 +0200

    iep_variant

--- lab10/list_sequence_counter_var2.py
+++ lab10/list_sequence_counter_var2.py
@@ -3,18 +3,26 @@
 	Пример: {2, 1, 1, 2, 3, 3, 2, 2, 2, 1} -> {2, 2, 2}. ‘2’ * counter
 """
 
-l = [2, 3, 3, 1, 1, 2, 3, 3, 2, 2, 2, 1]
+l = [2, 2, 2, 1, 3, 3, 3, 3, 2, 2, 2, 1]
 
+max_sequence_count = 0
+current_sequence_count = 0
+max_sequence_element = -1
 
 
 for i in range(len(l)):
-	print(f'\ni = {i}')
-	print('*' * 20,)
 
-	sub_l = l[i+1:]
+	for j in range(i+1,len(l)):
+		if l[i]==l[j]:
+			current_sequence_count+=1
+		else:
+			break
 
-	for j in range(len(sub_l)):
-		print(f'{j} = {sub_l[j]}')
+	if current_sequence_count>max_sequence_count:
+		max_sequence_count = current_sequence_count
+		max_sequence_element = l[i]
+
+	current_sequence_count = 0
 
 
-
+print( str(max_sequence_element) * max_sequence_count)
