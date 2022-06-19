0e70788145939009f1b3bfe8ace888a7210cbcc9
WWWCourses <ivawebcourses@gmail.com>
2022-06-19 21:08:54 +0300

    Lab32: 19.06.2022 DONE: https://wwwcourses.github.io/PythonCourseNetIT-Slides/pages/themes/ParallelProgramming-Lecture2/ParallelProgramming-Lecture2.html#/5/3

--- lab32/critical_section.py
+++ lab32/critical_section.py
@@ -3,31 +3,25 @@ import time
 
 
 def worker():
-	# TODO: why counter not locked
-	lock.acquire()
-	global counter
+  global counter
 
-	for i in range(10000000):
-		counter += 1
-	lock.release()
-
+  for i in range(10000000):
+    counter += 1
 
 
 counter = 0
 
 # create some treads to count together:
 thread_pool = []
-lock = threading.Lock()
-
 for i in range(2):
-	tr = threading.Thread(target=worker)
-	thread_pool.append(tr)
+  tr = threading.Thread(target=worker)
+  thread_pool.append(tr)
 
-	print(f"Counter before start of {tr.name}: {counter}")
-	tr.start()
+  print(f"Counter before start of {tr.name}: {counter}")
+  tr.start()
 
 # wait for tread to finish:
 for tr in thread_pool:
-	tr.join()
+  tr.join()
 
 print("Workers counted:", counter)
