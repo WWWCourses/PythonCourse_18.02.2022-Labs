9e0df7b7690d30d0561d4d4c52e9b5a7c9f4bf4c
WWWCourses <ivawebcourses@gmail.com>
2022-10-21 21:21:02 +0300

    Lab51: 21.10.2022 DONE: https://wwwcourses.github.io/PythonCourseNetIT-Slides/pages/themes/PyQT-Lecture8/PyQT-Lecture8.html#/7

--- lab51/styleSheetDemo.py
+++ lab51/styleSheetDemo.py
@@ -10,17 +10,47 @@ class MainWindow(qtw.QWidget):
 		super().__init__(*args, **kwargs)
 
 		lbl1 = qtw.QLabel('Welcome')
-		btnSubmit = qtw.QPushButton('Submit')
+		self.btnSubmit = qtw.QPushButton('Submit')
+		btnCancel = qtw.QPushButton('Cancel')
+		btnCancel.setObjectName('btnCancel')
+
+		btnCancel.setStyleSheet("""
+			background-color:purple;
+			padding:10px;
+		""")
+
+		btnCheck = qtw.QCheckBox()
+		btnRadio = qtw.QRadioButton()
+
 
 		mainLayout = qtw.QVBoxLayout(self)
 		mainLayout.addWidget(lbl1)
-		mainLayout.addWidget(btnSubmit)
+		# mainLayout.addWidget(self.btnSubmit)
+		mainLayout.addWidget(btnCancel)
 
+		hLayout = qtw.QHBoxLayout()
+		hLayout.addWidget(btnCheck)
+		hLayout.addWidget(self.btnSubmit)
+		mainLayout.addLayout(hLayout)
+		mainLayout.addWidget(btnRadio)
+
+		btnCheck.clicked.connect(self.changeStyle)
 
 		self.applyStyleSheet()
 
 		self.setWindowTitle('Style Sheet Demo')
 		self.show()
+
+	def changeStyle(self, checked):
+		if checked:
+			self.btnSubmit.setStyleSheet("""
+				font-size:50px
+			""")
+		else:
+			self.btnSubmit.setStyleSheet("""
+				font-size:10px
+			""")
+
 
 	def applyStyleSheet(self):
 		with open('./main.css','r') as f:
@@ -33,5 +63,6 @@ if __name__ == '__main__':
 	app = qtw.QApplication(sys.argv);
 
 	window = MainWindow()
+
 
 	sys.exit(app.exec())
